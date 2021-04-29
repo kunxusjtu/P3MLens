51a48163d1bd99d9e0cf9954e0db6cdda89dbc8d
Kun Xu <33591562+kunxusjtu@users.noreply.github.com>
2021-04-29 21:51:41 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,6 +37,28 @@ setup(
     license='MIT',
     description='An Accurate P3M Algorithm for Gravitational Lensing Studies in Simulations.',
     long_description=read('README.md'),
-    install_requires=['numpy', 'scipy', 'numba', 'astropy],
+    install_requires=['numpy', 'scipy>=1.6.0', 'numba', 'astropy],
     tests_require=['pytest', 'pytest-xdist'],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Astronomy',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
